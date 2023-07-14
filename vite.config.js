1a67795e939777a4b99efd3ee1444c7b02af809f
BubuDavid <david.pedroza.segoviano@gmail.com>
2023-07-14 12:41:31 -0600

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	base: '/react-router-testing/',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/React-Router-testing/'
+  }
+
+  return config
 })
